d564401bf34b20f51e6a7e8f07f6451323c045d3
Josh Grossman <grossmanjoshuaj@gmail.com>
2019-10-25 10:37:09 -0500

    added input for instrument and genre and conditional display of posts based on login

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const PostCard = ({ title, blurb, onClick }) => {
+const PostCard = ({ title, blurb, onClick, genre, instrument}) => {
     return(
         
         <div className="card" style={{width: '18rem'}}>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
+              <h5 className="card-title">{genre}</h5>
+              <h5 className="card-title">{instrument}</h5>
                 <p className="card-text">{blurb}</p>
                 <button 
                 name="delete" 
