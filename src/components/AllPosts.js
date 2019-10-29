8e5c27c87013533385e4e9856a3cefe828507859
Sonmi Kos <sonmi.kos@gmail.com>
2019-10-28 23:10:42 -0500

    applied React Material UI card style to the postcard component

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,75 +1,55 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import { connect } from 'react-redux';
-import { allPosts } from '../actions/postAction';
-import { getUser } from '../actions/userAction';
-import PostCard  from './PostCard';
-
-
+import React, { Component } from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { allPosts } from "../actions/postAction";
+import { getUser } from "../actions/userAction";
+import PostCard from "./PostCard";
+import Container from "@material-ui/core/Container";
 
 class AllPosts extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      title: "",
-      blurb: "",
-      instrument:"",
-      genre:"",
-      
-      
-      
-      
-    };
+    this.state = {};
     //bind
     this.renderPosts = this.renderPosts.bind(this);
   }
 
   // lifecycle methods
   componentDidMount() {
-   
-   this.props.allPosts();
-   this.props.getUser();
+    this.props.allPosts();
+    this.props.getUser();
   }
-  
 
   //render posts
-  renderPosts(){
+  renderPosts() {
     console.log(this.props.posts);
     return _.map(this.props.posts, (post, key) => {
-      return(
-        <PostCard 
-        key={key} 
-        title={post.title}
-        instrument={post.instrument} 
-        genre={post.genre}
-        blurb={post.blurb}>
-        </PostCard>
-       
-      )
-    })
+      return (
+        <PostCard
+          key={key}
+          title={post.title}
+          instrument={post.instrument}
+          genre={post.genre}
+          blurb={post.blurb}
+        ></PostCard>
+      );
+    });
   }
 
   render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12">
-            
-            {this.renderPosts()}
-          </div>
-        </div>
-      </div>
-    );
+    return <Container maxWidth="sm">{this.renderPosts()}</Container>;
   }
-
 }
 
-function mapStateToProps(state, ownProps){
-  return{
+function mapStateToProps(state, ownProps) {
+  return {
     posts: state.posts,
     user: state.user
-  }
+  };
 }
 
-export default connect(mapStateToProps, { allPosts, getUser })(AllPosts);
+export default connect(
+  mapStateToProps,
+  { allPosts, getUser }
+)(AllPosts);
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,38 +1,100 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    marginTop: "30px"
+  },
+  media: {
+    height: 140
+  }
+});
 
-const PostCard = ({ title, blurb, onClick, genre, instrument }) => {
+export default function MediaCard({
+  title,
+  blurb,
+  onClick,
+  genre,
+  instrument
+}) {
+  const classes = useStyles();
+
   return (
-
-    <div className="media" >
-      <a href="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
-      target="blank">
-      <img src="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
-        className="mr-3"
-        alt="..."
-        style={{ width: '100px', height: '100px' }} />
+    <Card className={classes.card}>
+      <CardActionArea>
+        <a
+          href="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
+          target="_blank"
+        >
+          <CardMedia
+            className={classes.media}
+            image="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
+            title=""
+          />
         </a>
-        
-      <div className="media-body">
-        <h3 className="mt-0">{title}</h3>
-        <h6 className="card-title">{genre}</h6>
-        <h6 className="card-title">{instrument}</h6>
-        {blurb}
-        <hr />
-        {(onClick == null)? null : <button
-          name="delete"
-          onClick={onClick}
-          className="btn btn-danger">Delete</button>
-        }
-      </div>
-    </div>
 
-
-
-
-
-
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <div className="genre">{genre}</div>
+            <div className="instrument">{instrument}</div>
+            <div className="blurb">{blurb}</div>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+        {onClick && (
+          <Button size="small" color="primary" onClick={onClick}>
+            Delete
+          </Button>
+        )}
+      </CardActions>
+    </Card>
   );
 }
 
-export default PostCard;
+// const PostCard = ({ title, blurb, onClick, genre, instrument }) => {
+//   return (
+
+//     <div className="media" >
+//       <a href="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
+//       target="blank">
+//       <img src="https://firebasestorage.googleapis.com/v0/b/sheet-share-41538.appspot.com/o/Bohemian_Rhapsody.png?alt=media&token=d6817355-f4ee-489b-8a16-e3f86d97c75d"
+//         className="mr-3"
+//         alt="..."
+//         style={{ width: '100px', height: '100px' }} />
+//         </a>
+
+//       <div className="media-body">
+//         <h3 className="mt-0">{title}</h3>
+//         <h6 className="card-title">{genre}</h6>
+//         <h6 className="card-title">{instrument}</h6>
+//         {blurb}
+//         <hr />
+//         {(onClick == null)? null : <button
+//           name="delete"
+//           onClick={onClick}
+//           className="btn btn-danger">Delete</button>
+//         }
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default PostCard;
